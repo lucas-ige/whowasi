311181c58fd3502b36edc18e54ce3f5fb50f8e5e
Lucas Bastien <134368131+lucas-ige@users.noreply.github.com>
2026-07-29 17:09:56 +0200

    Add useful wrappers for running subprocesses

---
+++ src/whowasi/_utils.py
@@ -0,0 +1,66 @@
+# whowasi: a tool to record source version information into binaries.
+#
+# Copyright (c) 2026 Institut des Géosciences de l'Environnement, Grenoble.
+#
+# License: BSD 3-clause "new" or "revised" license (BSD-3-Clause).
+
+import subprocess
+
+
+def run(args, **kwargs):
+    """Run given command and arguments as a subprocess.
+
+    Parameters
+    ----------
+    args: sequence
+        The command to run and its arguments, eg. ["grep", "-v", "some text"].
+    kwargs: dict
+        These are passed "as is" to subprocess.run.
+
+    Returns
+    -------
+    subprocess.CompletedProcess
+        The result of running the command.
+
+    Raises
+    ------
+    RuntimeError
+        If the command returns a non-zero exit code.
+
+    """
+    out = subprocess.run(args, **kwargs)
+    if out.returncode:
+        msg = f"Command '{' '.join(args)}' exited with non-zero return code."
+        raise RuntimeError(msg)
+    return out
+
+
+def run_stdout(args, **kwargs):
+    """Run given command and arguments as a subprocess and return stdout.
+
+    Parameters
+    ----------
+    args: sequence
+        The command to run and its arguments, eg. ["grep", "-v", "some text"].
+    kwargs: dict
+        These are passed "as is" to subprocess.run, but cannot contain
+        "capture_output" nor "text".
+
+    Returns
+    -------
+    [str]
+        The standard output of the command (one string per line).
+
+    Raises
+    ------
+    RuntimeError
+        If the command returns a non-zero exit code.
+
+    """
+    nope_list = ("capture_output", "text")
+    forbidden_kwargs = list(filter(lambda kwarg: kwarg in nope_list, kwargs))
+    if forbidden_kwargs:
+        msg = f"Forbidden keyword argument(s): {', '.join(forbidden_kwargs)}."
+        raise ValueError(msg)
+    out = run(args, capture_output=True, text=True, **kwargs)
+    return out.stdout[:-1].split("\n")
